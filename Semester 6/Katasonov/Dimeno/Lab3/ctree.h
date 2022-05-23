49925539066e78ccf4ab579f73503b2a7ef22348
vladislick <vladislickness@gmail.com>
2022-05-23 22:43:23 +0700

    Laba is completed

--- Semester 6/Katasonov/Dimeno/Lab3/ctree.h
+++ Semester 6/Katasonov/Dimeno/Lab3/ctree.h
@@ -15,21 +15,33 @@ private:
         Node    *pLeft;
         // Указатель на правый узел
         Node    *pRight;
+        // Конструктор узла
+        Node(const Tkey &KEY, const Tdata& DATA) {
+            key = KEY;
+            data = DATA;
+            pLeft = pRight = nullptr;
+        }
     };
     
     // Указатель на начало списка
     Node *pRoot;
 
-    // Функция обхода дерева, начиная с root, методом TLR
-    void TLR(Node* root, void (ctree<Tdata,Tkey>::*func)(Node*)) {
+public:
+    // Конструктор по умолчанию
+    ctree() {
+        pRoot = nullptr;
+    }
+
+    // Конструктор копирования с обходом TLR
+    ctree(const ctree& tree) {
         // Проверяем, существует ли дерево
-        if (root == nullptr) return;
+        if (tree.pRoot == nullptr) return;
         // Для обхода дерева создаем стэк
         std::list<Node*> stack;
         // Вспомогательный указатель        
         Node *pTemp;
         // Кладем в стэк указатель на начало дерева
-        stack.push_back(root);
+        stack.push_back(tree.pRoot);
         // В цикле обрабатываем стэк
         while (!stack.empty()) {
             // Достаем из стэка адрес узла
@@ -41,76 +53,64 @@ private:
             if (pTemp->pLeft != nullptr)
                 stack.push_back(pTemp->pLeft);
             // Обработка узла
-            (this->*func)(pTemp);
+            insert(pTemp->key, pTemp->data);
         }
-    }
-
-    // Функция удаления узла
-    void destructor_Func(Node* node){
-        delete node;
-    } 
-
-public:
-    // Конструктор по умолчанию
-    ctree() {
-        pRoot = nullptr;
     }
 
     // Деструктор класса с методом обхода TLR
     ~ctree() {
-        // Обходим дерево, удаляя каждый узел
-        TLR(pRoot, &ctree<Tdata,Tkey>::destructor_Func);
+        // Проверяем, существует ли дерево
+        if (pRoot == nullptr) return;
+        // Для обхода дерева создаем стэк
+        std::list<Node*> stack;
+        // Вспомогательный указатель        
+        Node *pTemp;
+        // Кладем в стэк указатель на начало дерева
+        stack.push_back(pRoot);
+        // В цикле обрабатываем стэк
+        while (!stack.empty()) {
+            // Достаем из стэка адрес узла
+            pTemp = stack.back(); stack.pop_back();
+            // Добавляем в стек адрес правого узла при наличии
+            if (pTemp->pRight != nullptr)
+                stack.push_back(pTemp->pRight);
+            // Добавляем в стек адрес левого узла при наличии
+            if (pTemp->pLeft != nullptr)
+                stack.push_back(pTemp->pLeft);
+            // Обработка узла
+            delete pTemp;
+        }
+        // Очищаем указатель на начало списка
+        pRoot = nullptr;
     } 
 
     // Втавить новый узел в дерево
     void insert(const Tkey& KEY, const Tdata &DATA) {
         // Создаем временный указатель на узел и кладем адрес начала дерева
         Node *temp = pRoot;
-        // В цикле проходим по дереву
-        while(temp != nullptr) {
-            if (temp->key > KEY) temp = temp->pLeft;
-            else if (temp->key < KEY) temp = temp->pRight;
-            else break;
+        // Если дерево пустое
+        if (temp == nullptr) {
+            pRoot = new Node(KEY, DATA);
+            return;
         }
-    }
-
-    // Конструктор копирования с обходом TLR
-    ctree(const ctree& tree) {
-        // Если копируемое дерево пустое, то выходим
-        if (tree.pRoot == nullptr) return;
-        // Для обхода копируемого дерева создаем стэк
-        std::list<Node*> stack;
-        std::list<Node**> stackCopy;
-        // Вспомогательные указатели
-        Node *pTemp, **pCopyTemp;
-        // Кладем в стэк указатель на начало копируемого дерева
-        stack.push_back(tree.pRoot);
-        // Кладем в стэк указатель на начало копируемого дерева
-        stackCopy.push_back(&pRoot);
-        // В цикле обрабатываем стэк
-        while (!stack.empty()) {
-            // Достаем из стэка адрес узла
-            pTemp = stack.back(); stack.pop_back();
-            pCopyTemp = stackCopy.back(); stack.pop_back();
-            // Создаем первый узел для дерева
-            (*pCopyTemp) = new Node();
-            // Копируем данные в новый узел
-            (*pCopyTemp)->key = pTemp->key;
-            (*pCopyTemp)->data = pTemp->data;
-            (*pCopyTemp)->pLeft = (*pCopyTemp)->pRight = nullptr;
-            // Создаем дочерний узел справа при наличии
-            if (pTemp->pRight != nullptr) {
-                // Заносим узел копируемого дерева в стэк
-                stackCopy.push_back(&((*pCopyTemp)->pRight));
-                // Заносим узел копируемого дерева в стэк
-                stack.push_back(pTemp->pRight);
-            }
-            // Создаем дочерний узел слева при наличии
-            if (pTemp->pLeft != nullptr) {
-                // Заносим узел копируемого дерева в стэк
-                stackCopy.push_back(&((*pCopyTemp)->pLeft));
-                // Заносим узел копируемого дерева в стэк
-                stack.push_back(pTemp->pLeft);
+        // В цикле проходим по дереву
+        while(true) {
+            if (temp->key > KEY) {
+                if (temp->pLeft != nullptr) temp = temp->pLeft;
+                else {
+                    temp->pLeft = new Node(KEY, DATA);
+                    break;
+                }
+            } else if (temp->key < KEY) {
+                if (temp->pRight != nullptr) temp = temp->pRight;
+                else {
+                    temp->pRight = new Node(KEY, DATA);
+                    break;
+                }
+            } else {
+                temp->data = DATA;
+                temp->key = KEY;
+                break;
             }
         }
     }
@@ -128,5 +128,132 @@ public:
         }
         return false;
     }
+
+    // Поиск нужного узла
+    bool searchByData(Tkey& KEY, const Tdata &DATA) {
+        // Проверяем, существует ли дерево
+        if (pRoot == nullptr) return false;
+        // Для обхода дерева создаем стэк
+        std::list<Node*> stack;
+        // Вспомогательный указатель        
+        Node *pTemp;
+        // Кладем в стэк указатель на начало дерева
+        stack.push_back(pRoot);
+        // В цикле обрабатываем стэк
+        while (!stack.empty()) {
+            // Достаем из стэка адрес узла
+            pTemp = stack.back(); stack.pop_back();
+            // Добавляем в стек адрес правого узла при наличии
+            if (pTemp->pRight != nullptr)
+                stack.push_back(pTemp->pRight);
+            // Добавляем в стек адрес левого узла при наличии
+            if (pTemp->pLeft != nullptr)
+                stack.push_back(pTemp->pLeft);
+            if (pTemp->data == DATA) {
+                KEY = pTemp->key;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    bool erase(const Tkey& KEY) {
+        // Указатель на текущий узел
+        Node *temp = pRoot;
+        // Адрес переменной, которая хранит адрес на текущий узел
+        Node **prev = &pRoot;
+        while(true) {
+            // Если выпали на несуществующий узел
+            if (temp == nullptr) return false;
+            // Узнаем адрес с указателем
+            else if (temp->key > KEY) {
+                prev = &(temp->pLeft);
+                temp = temp->pLeft;
+            }
+            else if (temp->key < KEY) {
+                prev = &(temp->pRight);
+                temp = temp->pRight;
+            }
+            // Если находимся на нужном узле
+            else {
+                // Если у удаляемого узла нет потомков
+                if (temp->pLeft == nullptr && temp->pRight == nullptr) {
+                    *prev = nullptr;
+                    delete temp;
+                } 
+                // Если у удаляемого узла два потомка
+                else if (temp->pLeft != nullptr && temp->pRight != nullptr) {
+                    // Поиск замещающего узла
+                    Node *t = temp, **tprev;
+                    tprev = &(t->pRight);
+                    t = t->pRight;
+                    while(t->pLeft != nullptr) {
+                        tprev = &(t->pLeft);
+                        t = t->pLeft;
+                    }
+                    // Копирование данных из замещающего узла
+                    temp->key = t->key;
+                    temp->data = t->data;
+                    // Восстановление связи
+                    *tprev = t->pRight;
+                    // Удаление замещающего узла
+                    delete t;
+                } 
+                // Если у удаляемого узла только левый потомок
+                else if (temp->pLeft != nullptr) {
+                    *prev = temp->pLeft;
+                    delete temp;
+                } 
+                // Если у удаляемого узла только правый потомок
+                else if (temp->pRight != nullptr) {
+                    *prev = temp->pRight;
+                    delete temp;
+                }
+                return true;
+            }
+        }
+    }
     
+    // Вывести все узлы на заданной глубине дерева
+    void atLevel(unsigned int level, std::list<Tkey> &nodes) {
+        // Проверяем, существует ли дерево
+        if (pRoot == nullptr) return;
+        // Очищаем список узлов
+        nodes.clear();
+        // Для обхода дерева создаем стэк
+        std::list<Node*> stack;
+        // Глубина узла
+        unsigned int tLevel;
+        // Искомый ключ
+        Tkey neededKey;
+        // Вспомогательный указатель        
+        Node *pTemp;
+        // Кладем в стэк указатель на начало дерева
+        stack.push_back(pRoot);
+        // В цикле обрабатываем стэк
+        while (!stack.empty()) {
+            // Достаем из стэка адрес узла
+            pTemp = stack.back(); stack.pop_back();
+            // Добавляем в стек адрес правого узла при наличии
+            if (pTemp->pRight != nullptr)
+                stack.push_back(pTemp->pRight);
+            // Добавляем в стек адрес левого узла при наличии
+            if (pTemp->pLeft != nullptr)
+                stack.push_back(pTemp->pLeft);
+            // Вычисляем уровень найденного узла
+            tLevel = 0;
+            // Запоминаем ключ найденного узла
+            neededKey = pTemp->key;
+            // Начинаем с начала дерева (0-ой уровень)
+            pTemp = pRoot;
+            while(neededKey != pTemp->key) {
+                if (neededKey > pTemp->key) pTemp = pTemp->pRight;
+                else pTemp = pTemp->pLeft;
+                tLevel++;
+            }
+            // Если глубина соответствует, то добавляем в список
+            if (tLevel == level) nodes.push_back(neededKey);
+        }
+    }
+
 };
--- Semester 6/Katasonov/Dimeno/Lab3/main.cpp
+++ Semester 6/Katasonov/Dimeno/Lab3/main.cpp
@@ -4,6 +4,55 @@
 using namespace std;
 
 int main() {
-    ctree<int, int> tree, tree2(tree);
+    ctree<int, int> tree;
+    int data;
+    tree.insert(10, 1);
+    tree.insert(4, 2);
+    tree.insert(15, 3);
+    tree.insert(12, 4);
+    tree.insert(11, 5);
+    tree.insert(14, 6);
+    tree.insert(17, 7);
+    tree.insert(16, 8);
+    tree.insert(20, 9);
+    tree.insert(21, 10);
+    ctree<int, int> tree2(tree);
+    tree.~ctree();
+
+    printf("Before: \n");
+    list<int> listing; 
+    int i = 0;
+    while (true) {
+        tree2.atLevel(i, listing);
+        if (listing.size() == 0) break;
+        printf("L%d: ", i);
+        for (auto it = listing.begin(); it != listing.end(); it++) {
+            if (it != listing.begin()) printf(", ");
+            printf("%d", *it);
+        }
+        printf(".\n");
+        i++;
+    }
+
+    tree2.erase(10);
+
+    printf("After: \n");
+    i = 0;
+    while (true) {
+        tree2.atLevel(i, listing);
+        if (listing.size() == 0) break;
+        printf("L%d: ", i);
+        for (auto it = listing.begin(); it != listing.end(); it++) {
+            if (it != listing.begin()) printf(", ");
+            printf("%d", *it);
+        }
+        printf(".\n");
+        i++;
+    }
+
+    int key;
+    if (tree2.searchByData(key, 7)) printf("Found node with key %d\n", key);
+
+    
     return 0;
 }
